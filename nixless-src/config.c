5f4f7699195abef5bebb8d3a50e957e074ffd881
SP649 <158019744+lovemearowlet@users.noreply.github.com>
2024-11-30 22:03:00 -0500

    Update config.c

--- nixless-src/config.c
+++ nixless-src/config.c
@@ -3,6 +3,7 @@
 
     Nixless Config:
     pkgin("build thing here") but replace build thing with the steps to build
+    ie: pkgin("git clone https://github.com/dylanaraps/neofetch && cd neofetch && make install); for neofetch
 
 
 */
